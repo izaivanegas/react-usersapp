8cee02d6771289c39a72a77c905c0bcd139ee220
izai <izai.vanegas@gmail.com>
2026-07-25 08:01:37 -0600

    Instale bootstrap y limpie el proyecto para iniciar a codificar

---
+++ src/UsersApp.jsx
@@ -0,0 +1,13 @@
+
+
+export const UsersApp = () => {
+
+    return (
+        <>
+            <div className="container">
+                <h1>Users App</h1>
+            </div>
+        </>
+    );
+
+}
