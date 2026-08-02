5e2030949a1a01a0ea754452aabf6b015271f9c7
izaivanegas <62458337+izaivanegas@users.noreply.github.com>
2026-08-01 18:42:41 -0600

    Agregue el modal usando bootstrap para poder tener le formulario en una ventana independiente

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -20,22 +20,34 @@ export const UsersApp = () => {
     return (
         <>
 
+            { !visibleForm || <div className="abrir-modal fadeIn">
+                <div className="modal" style={{display:"block"}} tabIndex="-1">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" >
+                                    {userSelected.id >0 ? 'Editar': 'Crear'} Modal Usuarios
+                                </h5>
+                            </div>
+                            <div className="modal-body">
+                                <UserForm
+                                    handlerAddUser={handlerAddUser}
+                                    initialUserForm={initialUserForm}
+                                    userSelected={userSelected}
+                                    handleCloseForm={handleCloseForm}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            }
+
             <div className="container my-4">
                 <Header/>
                 <div className="row">
 
-                        {
-                            !visibleForm ||
 
-                            <div className="col">
-                            <UserForm
-                                handlerAddUser={handlerAddUser}
-                                initialUserForm={initialUserForm}
-                                userSelected={userSelected}
-                                handleCloseForm={handleCloseForm}
-                            />
-                            </div>
-                        }
 
                     <div className="col">
 
