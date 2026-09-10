8d0de669f2b6fd71560031576969ff56979ae97c
izaivanegas <62458337+izaivanegas@users.noreply.github.com>
2026-09-10 17:38:32 -0600

    fix: Se ajusta una comparacion de abstracta a estricta

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -16,7 +16,7 @@ export const RegisterPage = () => {
 
     useEffect(()=>{
         if(id){
-            const user = users.find(user => user.id == id)
+            const user = users.find(user => user.id === id)
             if (user){
                 setUserSelected(user)
             }else{
